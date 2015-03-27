9b8908c6a6a4dd9ee5df207657a55d2be890cb40
nevinm <nevinm@qburst.com>
2015-03-27 11:30:05 +0530

    Added js to make slider responsive.

--- web/js/main.js
+++ web/js/main.js
@@ -4,10 +4,18 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
     	css3: true,
     	scrollingSpeed:1000,
-    	responsive: 1,
+    	responsive: 1400,
     	slidesNavigation: true,
     	controlArrows: false,
     	navigation:true
-
+    });
+    $(window).resize(function(){
+		var sliderImages = $("#fullpage").find("img");
+		$.each(sliderImages,function(index,elem){
+			if(elem.height<window.innerHeight){
+				elem.height=window.innerHeight;
+			}
+			else{}
+		});
     });
 });
