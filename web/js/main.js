701097e1ad28184edbe2d0f37ffdbdc7c50baa02
aditya <aditya@qburst.com>
2015-03-30 12:01:01 +0530

    menu page

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,16 @@
 
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-    	css3: true,
-    	scrollingSpeed:1000,
-    	responsive: 1,
-    	slidesNavigation: true,
-    	controlArrows: false,
-    	navigation:true
+    $(function(){
+    if($("#fullpage").length){
+        $('#fullpage').fullpage({
+        	css3: true,
+        	scrollingSpeed:1000,
+        	responsive: 1,
+        	slidesNavigation: true,
+        	controlArrows: false,
+        	navigation:true
+        });
+    }
     });
     $('.icon-menu').on("click",function (e){
     	e.stopPropagation(); 
---
+++ web/js/menu.js
@@ -0,0 +1,8 @@
+$(function(){
+        if($(".subMenu").length){
+        $(".subMenu ul li a").on("click",function(){
+            $(".subMenu ul li a").removeClass("activeOption");
+            $(this).addClass("activeOption");
+        });
+        }
+    });
