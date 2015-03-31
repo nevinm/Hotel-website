40f44fa24c0b25b1b1b6053dd0d6abbfedf2651d
nevinm <nevinm@qburst.com>
2015-03-31 16:36:15 +0530

    Integrated Login API

--- web/js/ajax_reusable.js
+++ web/js/ajax_reusable.js
@@ -23,6 +23,7 @@ AjaxHttpSender.prototype.sendPost = function(url, data, callback) {
    $.ajax({
       url: url,
       type: 'POST',
+      // header: header,
       data: data,
       beforeSend: function() {
          onStartAjaxRequest();
@@ -48,17 +49,4 @@ function onEndAjaxRequest() {
 }
 
 
-//How to use the ajax reusable component
- var callback = {
-      success: function(data, textStatus) {
-         $('#content').html(data);
-      },
-      failure: function(XMLHttpRequest, textStatus, errorThrown) {
-         alert('Error making AJAX call: ' + XMLHttpRequest.statusText + ' (' + XMLHttpRequest.status + ')');
-      }
-   }
- 
-   function makeAjaxCall() {
-      var ajaxHttpSender = new AjaxHttpSender();
-      ajaxHttpSender.sendGet(url, callback);
-   }
+
