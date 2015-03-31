7b9244370f6f9f33d25f34d1392f5d810ff53d39
nevinm <nevinm@qburst.com>
2015-03-31 16:42:53 +0530

    Separated login js

--- web/js/common.js
+++ web/js/common.js
@@ -1,33 +1,5 @@
 $(document).ready(function() {
-    var baseURL = 'http://meisterdish.qburst.com/backend/api/';
-    //Login process
-    var loginCallback = {
-        success: function(data, textStatus) {
-            debugger;
-        },
-        failure: function(XMLHttpRequest, textStatus, errorThrown) {}
-    }
-
-    $("#login button").on('click', function(){
-        loggingIn();
-    });
-
-    function loggingIn() {
-        var url = baseURL + "login/", header = {};
-        var username = $("#username").val(),
-        password=$("#password").val(), remember=1,
-        userData={
-            "username" : username,
-            "password" : password,
-            "remember" : remember
-        },
-        header= JSON.stringify(header);
-        data=JSON.stringify(userData);
-
-        var loginInstance = new AjaxHttpSender();
-        loginInstance.sendPost(url, data, loginCallback);
-    }
-
+    
     //Logout process
 
     // &NAVMENU - RESPONSIVE
