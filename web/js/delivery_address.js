94a75e4fd0025503ecb7083fedfc9b9c4b30c928
nevinm <nevinm@qburst.com>
2015-04-07 12:59:47 +0530

    Address autopopulation done

--- web/js/delivery_address.js
+++ web/js/delivery_address.js
@@ -1,32 +1,49 @@
   //Remove address API
-var removeAddressCallback = {
-    success: function(data, textStatus) {
-        var userDetails = JSON.parse(data);
-        if (userDetails.status == 1) {
-           debugger;
-        }
-         else {}
-    },
-    failure: function(XMLHttpRequest, textStatus, errorThrown) {}
-}
+  var removeAddressCallback = {
+      success: function(data, textStatus) {
+          var userDetails = JSON.parse(data);
+          if (userDetails.status == 1) {
+              debugger;
+          } else {}
+      },
+      failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+  }
 
-function removeAddress() {
-    var url = baseURL + "get_profile/",
-        header = {
-            "session-key": localStorage["session_key"]
-        },
-        userData = {
-            "get": 1
-        };
-    data = JSON.stringify(userData);
-    var removeAddressInstance = new AjaxHttpSender();
-    removeAddressInstance.sendPost(url, header, data, removeAddressCallback);
-}
+  function removeAddress() {
+      var url = baseURL + "get_profile/",
+          header = {
+              "session-key": localStorage["session_key"]
+          },
+          userData = {
+              "get": 1
+          };
+      data = JSON.stringify(userData);
+      var removeAddressInstance = new AjaxHttpSender();
+      removeAddressInstance.sendPost(url, header, data, removeAddressCallback);
+  }
 
-function autoPopulateAdressess(){
-    var userProfile=JSON.parse(localStorage['user_profile']);
-    $.each(userProfile.address_list,function(key,value){
-        debugger;
-    });
-}
-autoPopulateAdressess();
+  function autoPopulateAdressess() {
+    $("#editaddress-container ol").empty();
+    var userProfile = JSON.parse(localStorage['user_profile']);
+        $.each(userProfile.address_list, function(key, value) {
+            var userProfile = JSON.parse(localStorage['user_profile']);
+            $("#editaddress-container ol").append('<li class="address">' +
+            '<div class ="button">' +
+            '<a href="#" data-id="'+value.id+'" class="edit-button btn btn-small-secondary">EDIT</a>' +
+            '<a href="#" data-id="'+value.id+'" class="remove-button btn btn-small-secondary">REMOVE</a></div>' +
+            '<div class="address-content">' +
+            '<span class="address-name">' + value.name + '</span>' +
+            '<span class="address-street">' + value.street + ', ' + value.building + '</span>' +
+            '<span class="address-city">' + value.city + ', ' + value.state + '</span>' +
+            '<span class="address-mobile">' + value.phone + '</span>' +
+            '<span class="green primary-address">PRIMARY ADDRESS</span>' +
+            '<span class="non-primary-address">MAKE THIS YOUR PRIMARY ADDRESS</span>' +
+            '</div></li>');
+            if (value.is_primary == 1) {
+                $('.primary-address:last').addClass("show");
+              } else {
+                $('.non-primary-address:last').addClass("show");
+            }
+        });
+  }
+  autoPopulateAdressess();
