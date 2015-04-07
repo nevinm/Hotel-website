d740720aa638f1c5a080d63c61942b32cbb94c7d
nevinm <nevinm@qburst.com>
2015-04-07 14:41:37 +0530

    Fixed fb bug on no popup

--- web/js/delivery_address.js
+++ web/js/delivery_address.js
@@ -1,36 +1,42 @@
-  //Remove address API
-  var removeAddressCallback = {
-      success: function(data, textStatus) {
-          var userDetails = JSON.parse(data);
-          if (userDetails.status == 1) {
-              debugger;
-          } else {}
-      },
-      failure: function(XMLHttpRequest, textStatus, errorThrown) {}
-  }
+//Remove address API
+var removeAddressCallback = {
+    success: function(data, textStatus) {
+        var userDetails = JSON.parse(data);
+        if (userDetails.status == 1) {
+            debugger;
+        } else {}
+    },
+    failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+}
 
-  function removeAddress() {
-      var url = baseURL + "get_profile/",
-          header = {
-              "session-key": localStorage["session_key"]
-          },
-          userData = {
-              "get": 1
-          };
-      data = JSON.stringify(userData);
-      var removeAddressInstance = new AjaxHttpSender();
-      removeAddressInstance.sendPost(url, header, data, removeAddressCallback);
-  }
+function removeAddress(id) {
+    console.log("Are you sure you want to remove?");
+    var url = baseURL + "remove_address/",
+        header = {
+            "session-key": localStorage["session_key"]
+        },
+        userData = {
+            "id": id
+        };
+    data = JSON.stringify(userData);
+    var removeAddressInstance = new AjaxHttpSender();
+    removeAddressInstance.sendPost(url, header, data, removeAddressCallback);
+}
 
-  function autoPopulateAdressess() {
+$(document).on('click', '.remove-button', function() {
+    var deleteId = $(this).data().id;
+    removeAddress(deleteId);
+});
+
+function autoPopulateAdressess() {
     $("#editaddress-container ol").empty();
     var userProfile = JSON.parse(localStorage['user_profile']);
-        $.each(userProfile.address_list, function(key, value) {
-            var userProfile = JSON.parse(localStorage['user_profile']);
-            $("#editaddress-container ol").append('<li class="address">' +
+    $.each(userProfile.address_list, function(key, value) {
+        var userProfile = JSON.parse(localStorage['user_profile']);
+        $("#editaddress-container ol").append('<li class="address">' +
             '<div class ="button">' +
-            '<a href="#" data-id="'+value.id+'" class="edit-button btn btn-small-secondary">EDIT</a>' +
-            '<a href="#" data-id="'+value.id+'" class="remove-button btn btn-small-secondary">REMOVE</a></div>' +
+            '<a href="#" data-id="' + value.id + '" class="edit-button btn btn-small-secondary">EDIT</a>' +
+            '<a href="#" data-id="' + value.id + '" class="remove-button btn btn-small-secondary">REMOVE</a></div>' +
             '<div class="address-content">' +
             '<span class="address-name">' + value.name + '</span>' +
             '<span class="address-street">' + value.street + ', ' + value.building + '</span>' +
@@ -39,11 +45,35 @@
             '<span class="green primary-address">PRIMARY ADDRESS</span>' +
             '<span class="non-primary-address">MAKE THIS YOUR PRIMARY ADDRESS</span>' +
             '</div></li>');
-            if (value.is_primary == 1) {
-                $('.primary-address:last').addClass("show");
-              } else {
-                $('.non-primary-address:last').addClass("show");
-            }
-        });
-  }
-  autoPopulateAdressess();
+        if (value.is_primary == 1) {
+            $('.primary-address:last').addClass("show");
+        } else {
+            $('.non-primary-address:last').addClass("show");
+        }
+    });
+}
+autoPopulateAdressess();
+
+//Add address API
+var addAddressCallback = {
+    success: function(data, textStatus) {
+        var userDetails = JSON.parse(data);
+        if (userDetails.status == 1) {
+            debugger;
+        } else {}
+    },
+    failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+}
+
+function addAddress(id) {
+    var url = baseURL + "add_address/",
+        header = {
+            "session-key": localStorage["session_key"]
+        },
+        userData = {
+            "id": id
+        };
+    data = JSON.stringify(userData);
+    var addAddressInstance = new AjaxHttpSender();
+    addAddressInstance.sendPost(url, header, data, addAddressCallback);
+}
