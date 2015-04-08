892eff921ca9df9f06bb3353a2d509a96f9bef52
nevinm <nevinm@qburst.com>
2015-04-08 12:54:37 +0530

    Added get cities and states api

--- web/js/delivery_address.js
+++ web/js/delivery_address.js
@@ -94,9 +94,10 @@
         var $addressPopup = $(".addaddress-popup");
         var newAddress = {
             username: $addressPopup.find("input[name*='fullname']").val(),
-            phone: $addressPopup.find("input[name*='phonenum']").val(),
+            phone: $addressPopup.find("input[name*='phonenumber']").val(),
             zip: $addressPopup.find("input[name*='zip']").val(),
             street: $addressPopup.find("input[name*='street']").val(),
+            city_id: $addressPopup.find(".city-selector").val(),
             building: $addressPopup.find("input[name*='building']").val(),
             is_primary: $addressPopup.find("input[type*='checkbox']").val() == "on" ? 1 : 0,
         }
@@ -113,7 +114,7 @@
                 "name": newAddress.username,
                 "phone": newAddress.phone,
                 "zip": newAddress.zip,
-                "city_id": 6,
+                "city_id": newAddress.city_id,
                 "street": newAddress.street,
                 "building": newAddress.building,
                 "is_primary": newAddress.is_primary
@@ -128,8 +129,6 @@
         addAddress();
     })
 
-
-
     //Edit address API
     var editAddressCallback = {
         success: function(data, textStatus) {
@@ -142,14 +141,15 @@
         failure: function(XMLHttpRequest, textStatus, errorThrown) {}
     }
 
-    function populateAdressToForm(id) {
+    function populateAddressToForm(id) {
         var $addressPopup = $(".addaddress-popup"),
             deliveryAddressList = JSON.parse(localStorage['delivery_addressess']);
         $.each(deliveryAddressList.address_list, function(key, value){
             if(value.id == id){
                 $addressPopup.find("input[name*='fullname']").val(value.name);
-                $addressPopup.find("input[name*='phonenum']").val(value.phone);
+                $addressPopup.find("input[name*='phonenumber']").val(value.phone);
                 $addressPopup.find("input[name*='zip']").val(value.zip);
+                $addressPopup.find(".state-selector").val(value.state_id);
                 $addressPopup.find("input[name*='street']").val(value.street);
                 $addressPopup.find("input[name*='building']").val(value.building);
                 if(value.is_primary==1){
@@ -157,6 +157,7 @@
                 }else{
                     $addressPopup.find("input[type*='checkbox']").prop("checked", false);
                 }
+                $addressPopup.find(".city-selector").val(value.city_id);
             }
         });
     }
@@ -171,7 +172,7 @@
                 "name": newAddress.username,
                 "phone": newAddress.phone,
                 "zip": newAddress.zip,
-                "city_id": 6,
+                "city_id": newAddress.city_id,
                 "street": newAddress.street,
                 "building": newAddress.building,
                 "is_primary": newAddress.is_primary
@@ -184,4 +185,72 @@
     $("#savepopup-data").on('click', function() {
         var currentId = $(this).data().id;
         editAddress(currentId);
-    })
+    });
+
+
+// Get states API
+    var getStatesCallback = {
+        success: function(data, textStatus) {
+            var stateList = JSON.parse(data);
+            localStorage['delivery-states'] = data;
+            if (stateList.status == 1) {
+                $.each(stateList.state_list, function (index, value) {
+                    $('.state-selector').append($('<option/>', { 
+                        value: value.id,
+                        text : value.name,
+                    }));
+                });  
+            } else {}
+        },
+        failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+    }
+
+    function getStates() {
+        var url = baseURL + "get_states/",
+            header = {
+                "session-key": localStorage["session_key"]
+            },
+            userData = {
+                "search": "a"
+            };
+        data = JSON.stringify(userData);
+        var getStatesInstance = new AjaxHttpSender();
+        getStatesInstance.sendPost(url, header, data, getStatesCallback);
+    }
+
+    // Get City API
+    var getCitiesCallback = {
+        success: function(data, textStatus) {
+            var cityList = JSON.parse(data);
+            if (cityList.status == 1) {
+                $.each(cityList.city_list, function (index, value) {
+                    $('.city-selector').append($('<option/>', { 
+                        value: value.id ,
+                        text : value.name,
+                    }));
+                });  
+            } else {}
+        },
+        failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+    }
+
+    function getCities(stateId) {
+        var url = baseURL + "get_cities/",
+            header = {
+                "session-key": localStorage["session_key"]
+            },
+            userData = {
+                "state_id":stateId
+            };
+        data = JSON.stringify(userData);
+        var getCitiesInstance = new AjaxHttpSender();
+        getCitiesInstance.sendPost(url, header, data, getCitiesCallback);
+    }
+
+    $(document).on('change', '.state-selector', function(){
+        var stateSelectedId = $(this).val();
+        getCities(stateSelectedId);        
+    });
+
+   
+    getStates();
