52de5b3ff749d641a188b0fcffe63ac019afc09a
Abdul Nasar <nasarp@qburst.com>
2015-04-20 12:26:44 +0530

    asd

--- meisterdish_server/settings/__init__.py
+++ meisterdish_server/settings/__init__.py
@@ -187,6 +187,7 @@ LOGGING = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+MAIL_USE_TLS=True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'afmpmail2@gmail.com'
 EMAIL_HOST_PASSWORD = 'afmp@123mail2'
