dee23d0b57676e02583b2df30e7412f0130be1e8
nevinm <nevinm@qburst.com>
2015-04-20 09:50:58 +0530

    Fixed slider auto slide.

--- web/js/index.js
+++ web/js/index.js
@@ -4,20 +4,21 @@ $(document).ready(function() {
         slidesNavigation: true,
         controlArrows: false,
         navigation: true,
-        //  afterRender: function(){
-        //     idInterval = setInterval(function () {
-        //         $.fn.fullpage.moveSlideRight();
-        //     }, 7000);
-        // },
-
-        // afterLoad: function (anchorLink, index) {
-        //     if (index == 1 || index ==2) {
-        //         idInterval = setInterval(function () {
-        //             $.fn.fullpage.moveSlideRight();
-        //         }, 7000);
-        //     }else{
-        //         clearInterval(idInterval);
-        //     }
-        // }
+        afterRender: function(){
+            idInterval = setInterval(function () {
+                $.fn.fullpage.moveSlideRight();
+            }, 7000);
+        },
+        afterLoad: function (anchorLink, index) {
+            clearInterval(idInterval);
+            if (index == 1 || index ==2) {
+                idInterval = setInterval(function () {
+                    $.fn.fullpage.moveSlideRight();
+                }, 7000);
+            }
+        else{
+                clearInterval(idInterval);
+            }
+        }
     });
 });
