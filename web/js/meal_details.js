eebbdf1bb58ecef91a418e71d98468722fbe9952
nevinm <nevinm@qburst.com>
2015-05-14 15:40:37 +0530

    Fixed conflicts and removed tab js code (Unecessary code)

--- web/js/meal_details.js
+++ web/js/meal_details.js
@@ -1,65 +1,11 @@
 var ipadWidth = 768;
 $(document).ready(function() {
-    // $("#meal-tabs").tabs();
-    mobileJqueryUITab();
+    tabRendering();
 });
 
-function checkTabReplacement() {
-    //To do mobile rendering
-    if (window.innerWidth < ipadWidth && mobileRendered == false) {
-        mobileJqueryUITab();
-        replaceWord(true);
-        mobileRendered = true;
-    }
-    //To do pc rendering
-    else if (window.innerWidth >= ipadWidth && mobileRendered == true) {
-        pcJqueryUiTab();
-        replaceWord(false);
-        mobileRendered = false;
-    }
-}
 
-function replaceWord(isMobile) {
-    if (isMobile) {
-        $(".meal-tab-container").find('.pc-tab').each(function(key, value) {
-            $value = $(value);
-            replacedWord = $value.attr("data-mobile-link");
-            $value.html(replacedWord);
-        });
-    } else {
-        $(".meal-tab-container").find('.pc-tab').each(function(key, value) {
-            $value = $(value);
-            replacedWord = $value.attr("data-pc-link");
-            $value.html(replacedWord);
-        });
-    }
-}
-
-function isMobileRendered() {
-    if (window.innerWidth >= ipadWidth) {
-        mobileRendered = false;
-        pcJqueryUiTab();
-        replaceWord(mobileRendered);
-    } else {
-        mobileRendered = true;
-        mobileJqueryUITab();
-        replaceWord(mobileRendered);
-    }
-}
-
-function pcJqueryUiTab() {
-    destroyTabs();
-    $("#meal-tabs").tabs();
-}
-
-function destroyTabs() {
-    if ($("#meal-tabs").data("ui-tabs")) {
-        $("#meal-tabs").tabs('destroy');
-    }
-}
-
-function mobileJqueryUITab() {
-    destroyTabs();
+function tabRendering() {
+    // destroyTabs();
     var $tabs = $("#meal-tabs").tabs({
         activate: function(event, ui) {
             $tabs.tabs('option', 'hide', false);
@@ -102,9 +48,4 @@ function mobileJqueryUITab() {
 $('.meal-tab-container ul li').on("click",function(){
     $('.meal-tab-container ul li').removeClass('activeli');
     $(this).addClass('activeli');
-    // if ($(window).width() <= 767 && $(window).width() >= 320) {
-    //     var tabName = $(this).find('a').attr('data-pc-link');
-    //     $('.mobile-tab-header').empty();
-    //     $('.mobile-tab-header').append(tabName);
-    // }
 });
