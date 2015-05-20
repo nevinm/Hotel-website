2ac061554cfb15154a34feae2abe3d35ffdddb17
Nevin Madhukar K <nevinm@qburst.com>
2015-05-20 20:28:39 +0530

    FIxed issues on CMS list

--- web/views/admin/js/admin.js
+++ web/views/admin/js/admin.js
@@ -1,46 +1,45 @@
+//Admin Login
+var adminCallback = {
+        success: function(data, textStatus) {
+            var adminDetails = JSON.parse(data);
+            if(adminDetails.status==1){
+                var userName = adminDetails.user.first_name+ ' ' + adminDetails.user.last_name;
+                localStorage['username']=userName;
+                localStorage['session_key']=adminDetails.session_key;
+                localStorage['admin_loggedIn']=true;
+                localStorage['loggedIn']=false;
+                window.location.href = 'orderlist.html'
+            }
+            else{
+                alert(adminDetails.message);
+            }
+        },
+        failure: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log("Admin login Failed");
+            alert("failed, please try again.")
+        }
+    }
 
-    
+function adminLogin() {
+    var adminUserName = $("#adminlogin-email").val();
+        adminPassword = $("#adminlogin-password").val();
+    var url = baseURL + 'login/';
+    header = {},
+    params = {
+        "username": adminUserName,
+        "password": adminPassword,
+        "remember": 1,
+        "is_admin":1
+    }
+
+    data = JSON.stringify(params);
+
+    var loginAdminInstance = new AjaxHttpSender();
+    loginAdminInstance.sendPost(url, header, data, adminCallback);
+}
 $(document).ready(function() {
-      //Admin Login
-    var adminCallback = {
-            success: function(data, textStatus) {
-                var adminDetails = JSON.parse(data);
-                if(adminDetails.status==1){
-                    var userName = adminDetails.user.first_name+ ' ' + adminDetails.user.last_name;
-                    localStorage['username']=userName;
-                    localStorage['session_key']=adminDetails.session_key;
-                    localStorage['admin_loggedIn']=true;
-                    localStorage['loggedIn']=false;
-                    window.location.href = 'orderlist.html'
-                }
-                else{
-                    alert(adminDetails.message);
-                }
-            },
-            failure: function(XMLHttpRequest, textStatus, errorThrown) {
-                console.log("Admin login Failed");
-                alert("failed, please try again.")
-            }
-        }
-
-        function adminLogin() {
-            var adminUserName = $("#adminlogin-email").val();
-                adminPassword = $("#adminlogin-password").val();
-            var url = baseURL + 'login/';
-            header = {},
-            params = {
-                "username": adminUserName,
-                "password": adminPassword,
-                "remember": 1,
-                "is_admin":1
-            }
-
-            data = JSON.stringify(params);
-
-            var loginAdminInstance = new AjaxHttpSender();
-            loginAdminInstance.sendPost(url, header, data, adminCallback);
-        }
-        $("#login-button").on('click',function(){
+        $("#login-button").on('click',function(e){
+            e.preventDefault();
             adminLogin();
         });
 });
