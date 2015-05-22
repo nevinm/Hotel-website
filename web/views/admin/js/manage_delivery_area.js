30c2708932a16d22d914051fb01566d561714e8c
nabeel <nabeel@qburst.com>
2015-05-22 16:38:14 +0530

    cms manage delivery area is compleetd

--- web/views/admin/js/manage_delivery_area.js
+++ web/views/admin/js/manage_delivery_area.js
@@ -1,6 +1,7 @@
 var current_tr;
 $(document).ready(function() {
 	$("#add-new-delivery-area").on("click",function(){
+		$("#new-address").val("");
 		$("#add-delivery-area").show();
 		$("#edit-delivery-area").hide();
 		$(".header").text("ADD NEW DELIVERY AREA");
@@ -9,23 +10,30 @@ $(document).ready(function() {
 	$("#add-delivery-area").on("click",function(){
 		$('.popup-wrapper').hide();
 		var zip = $("#new-address").val();
-		manageDeliveryArea("","",zip);
-		populateAddedZip(zip);
-	});
-	$("#check-zip").on("click",function(){
-		$("span.check-delivery").hide();
-		var zip = $("#new-address").val();
-		checkDelivery(zip);
+		manageDeliveryArea("","",zip,"add");
 	});
 	$(document).on('click', '#delete-zip', function() {
+		
+		$('.popup-wrapper').hide();
 		var id = $(this).closest('tr').attr('data-id');
-		manageDeliveryArea("",id,"");
-		// $(this).closest('tr').remove();
+		$("#yes-button").attr('data-id',id)
+		$(".confirm-popup-wrapper").show();
+	});
+	$("#yes-button").on("click",function(){
+		var id = $(this).attr('data-id');
+		$(".confirm-popup-wrapper").hide();
+		manageDeliveryArea("",id,"","delete");
+	});
+
+	$("#no-button").on("click",function(){
+		$(".confirm-popup-wrapper").hide();
 	});
 	$(document).on('click', '#edit-zip', function() {
+		var zip = $(this).closest('tr').find('.zip-value').text();
 		$("#add-delivery-area").hide();
 		$("#edit-delivery-area").show();
 		$(".header").text("CHANGE DELIVERY AREA");
+		$("#new-address").val(zip);
 		$('.popup-wrapper').show();
 		current_tr = $(this).closest('tr');
 	});
@@ -33,7 +41,7 @@ $(document).ready(function() {
 		$('.popup-wrapper').hide();
 		var id = current_tr.attr('data-id'),
 			zip = $('#new-address').val();
-		manageDeliveryArea(id,"",zip);
+		manageDeliveryArea(id,"",zip,"edit");
 	})
 	getDeliveryAreas(1);
 });
@@ -60,14 +68,12 @@ function getDeliveryAreas(nextPage){
 }
 function populateDeliveryAreas(deliveryData){
     $('#delivery-area tbody tr').remove();
-    var count= 0;
     $.each(deliveryData.aaData, function(key, value) {
-        $('#delivery-area tbody').append("<tr data-id='" + count + "'>" +
-            "<td class='zip-value'>" + value + "</td>" +
+        $('#delivery-area tbody').append("<tr data-id='" + value.id + "'>" +
+            "<td class='zip-value'>" + value.zip + "</td>" +
             "<td>" + "<input type='button' class='btn btn-small-primary medium-green' id='edit-zip' value='EDIT'>" + "</td>" +
             "<td>" + "<input type='button' class='btn btn-small-primary medium-green' id='delete-zip' value='DELETE'>" + "</td>" +
             "</tr>");
-        count++;
     });
 
     $(".pagination").pagination({
@@ -83,18 +89,22 @@ function populateDeliveryAreas(deliveryData){
 
 //manage delivery areas
 var manageDeliveryAreaCallback = {
-	success: function(data, textStatus) {
+	success: function(data, textStatus, deliveryArea) {
 		var deliveryData = JSON.parse(data);
 		if(deliveryData.status == -1){
 			showPopupMessage(deliveryData);
+			return 0;
 		}else{
-			populateEditChanges();
-
+			if(deliveryArea == "delete"){
+				deleteZip(deliveryData.id);
+			}else{
+				getDeliveryAreas(1);
+			}
 		}
 	},
 	failure: function(XMLHttpRequest, textStatus, errorThrown) {}
 }
-function manageDeliveryArea(edit_id,delete_id,zip){
+function manageDeliveryArea(edit_id,delete_id,zip, deliveryArea){
 	var url = baseURL + 'manage_delivery_area/';
 	header = {
 		"session-key": localStorage['session_key']
@@ -106,36 +116,7 @@ function manageDeliveryArea(edit_id,delete_id,zip){
 	},
 	data = JSON.stringify(params);
 	var manageDeliveryArea = new AjaxHttpSender();
-	manageDeliveryArea.sendPost(url, header, data, manageDeliveryAreaCallback);
-}
-
-//check delivery
-var checkDeliveryCallback = {
-	success: function(data, textStatus) {
-		var deliveryData = JSON.parse(data);
-		if(deliveryData.status == 1){
-			$("span.check-delivery").text(deliveryData.message);
-			$("span.check-delivery").show();
-			$("#edit-delivery-area,#add-delivery-area").removeClass("button-disabled");
-		}else{
-			$("span.check-delivery").text(deliveryData.message);
-			$("span.check-delivery").show();
-		}
-	},
-	failure: function(XMLHttpRequest, textStatus, errorThrown) {}
-}
-function checkDelivery(zip){
-	var url = baseURL + 'check_delivery/';
-	header = {
-		"session-key": localStorage['session_key']
-	},
-	params = {
-		"zip":zip
-	},
-	data = JSON.stringify(params);
-
-	var checkDelivery = new AjaxHttpSender();
-	checkDelivery.sendPost(url, header, data, checkDeliveryCallback);
+	manageDeliveryArea.sendPost(url, header, data, manageDeliveryAreaCallback, deliveryArea);
 }
 
 function showPopupMessage(data){
@@ -145,14 +126,6 @@ function showPopupMessage(data){
                 $('.popup-message').hide();
             }, 2000);
 }
-function populateEditChanges(){
-	var newZip = $('#new-address').val();
-	current_tr.find('.zip-value').text(newZip);
-}
-function populateAddedZip(zip){
-	$('#delivery-area tbody').append("<tr>"+
-		 "<td class='zip-value'>" + zip + "</td>" +
-            "<td>" + "<input type='button' class='btn btn-small-primary medium-green' id='edit-zip' value='EDIT'>" + "</td>" +
-            "<td>" + "<input type='button' class='btn btn-small-primary medium-green' id='delete-zip' value='DELETE'>" + "</td>" +
-		+"</tr>")
+function deleteZip(delete_id){
+	$("tr[data-id='"+delete_id+"']").remove();
 }
