e5eea0abea23006184a7c48bdb1e52cd97bb8126
nabeel <nabeel@qburst.com>
2015-05-26 19:33:52 +0530

    complete how it works page design and implemented image slider in these pages

--- web/js/how_it_works.js
+++ web/js/how_it_works.js
@@ -4,15 +4,23 @@ $(document).ready(function() {
 	    $(this).find('a').addClass('activeli');
 	});
 	$(".how-it-works-tab").tabs();
-	
-	$('#left-arrow').on("click",function(){
-		$('#we-source,#our-suppliers').animate({
+	$('.left-arrow').on("click",function(){
+		$('#we-source,#you-cook,#our-suppliers,#what-you-get').animate({
 			left: '+=100%'
-		},600);
+		},"slow","easeOutQuart");
 	})
-	$('#right-arrow').on("click",function(){
-		$('#we-source,#our-suppliers').animate({
+	$('.right-arrow').on("click",function(){
+		$('#we-source,#you-cook,#our-suppliers,#what-you-get').animate({
 			left: '-=100%'
-		},600);
+		},"slow","easeOutQuart");
 	})
+	$('.sub-tab-container  ul li').on("click", function() {
+	    $('.sub-tab-container ul li a').removeClass('subactiveli');
+	    $(this).find('a').addClass('subactiveli');
+	});
+	$('.sub-tab-container li').on("click",function(){
+		$('#farm,#roamimg-acres,#blue-ribbon-fish,#plantation,#common-thread-farm').hide();
+		var clicked_id =$(this).find('a').attr('href');
+		$(clicked_id).show();
+	});
 });
