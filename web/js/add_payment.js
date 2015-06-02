b6354424bf9f6ba13cc18b57f6297148c1637307
Nevin Madhukar K <nevinm@qburst.com>
2015-06-02 16:24:04 +0530

    Implemented stripe basic integration.

--- web/js/add_payment.js
+++ web/js/add_payment.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	populateYear();
 	CartItemCount();
+	stripeIntegration();
 });
 
 function populateYear() {
         var currentYear = new Date().getFullYear();
         for (var i = 1; i <= 20; i++) {
-            $('#ExpYear').append("<option value='" + i + "'>" + currentYear + "</option>");
+            $('#ExpYear').append("<option value='" + currentYear + "'>" + currentYear + "</option>");
             currentYear = currentYear + 1;
         }
     }
--- web/js/checkout.js
+++ web/js/checkout.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
         $('.address-info').hide();
         $('.payment-method-guest-container').show();
     }
-    
+    stripeIntegration();
     setCurrentTime();
     populateYear();
-    
+
     var cartItems;
 
     //Remove cart items
@@ -86,7 +86,7 @@ $(document).ready(function() {
     function populateYear() {
         var currentYear = new Date().getFullYear();
         for (var i = 1; i <= 20; i++) {
-            $('#ExpYear').append("<option value='" + i + "'>" + currentYear + "</option>");
+            $('#ExpYear').append("<option value='" + currentYear + "'>" + currentYear + "</option>");
             currentYear = currentYear + 1;
         }
     }
@@ -975,3 +975,5 @@ function validateOrder() {
     }
     return true;
 }
+
+
