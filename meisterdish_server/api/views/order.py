93712e7262ad2917b7ad5ade13a8b5621f95747d
Abdul Nasar <nasarp@qburst.com>
2015-06-02 17:06:08 +0530

    again fixing issues with update order

--- meisterdish_server/api/views/order.py
+++ meisterdish_server/api/views/order.py
@@ -344,7 +344,7 @@ def update_order(request, data, user, order_id):
                 if not sent:
                     log.error("Failed to send order complete notification")
 
-        return json_response({"status":1, "message":"The order has been updated", "id":order_id+"."})
+        return json_response({"status":1, "message":"The order has been updated", "id":str(order_id)+"."})
     except Exception as e:
         log.error("Update order status : " + e.message)
         return custom_error("Failed to update the order.")
@@ -389,7 +389,7 @@ def send_order_confirmation_notification(order):
                "last_name" : user.last_name.title(),
                "status":order.status,
                "delivery_type":order.delivery_type,
-               "review_link":settings.SITE_URL + 'views/reviews.html?sess=' + order.session_key + '&oi=' + order.id
+               "review_link":settings.SITE_URL + 'views/reviews.html?sess=' + order.session_key + '&oi=' + str(order.id)
                }
         
         msg = render_to_string('order_confirmation_email_template.html', dic)
@@ -421,7 +421,7 @@ def send_order_complete_notification(order):
                "name" : user.last_name + " " + user.first_name,
                "status":order.status,
                "delivery_type":order.delivery_type,
-               "review_link":settings.SITE_URL + 'views/reviews.html?sess=' + order.session_key + '&oi=' + order.id,
+               "review_link":settings.SITE_URL + 'views/reviews.html?sess=' + order.session_key + '&oi=' + str(order.id),
                }
         
         msg = render_to_string('order_complete_email_template.html', dic)
