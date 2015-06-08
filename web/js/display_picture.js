1934e9c0132f4688e12816ef288fd9ef2b1917c3
nevinm <nevinm@qburst.com>
2015-06-08 10:54:53 +0530

    Fixed pic upload validation.

--- web/js/display_picture.js
+++ web/js/display_picture.js
@@ -13,25 +13,39 @@ $(document).ready(function() {
     $('#profile_picture_id').fileupload({
         add: function(e, data) {
             var acceptFileTypes = /^image\/(gif|jpe?g|png)$/i,
-                error = {};
+            error=[];
             if (data.originalFiles[0]['type'] && !acceptFileTypes.test(data.originalFiles[0]['type'])) {
-                error.message = "Please check the file type";
+                error.message = 'Not an accepted file type';
                 showPopup(error);
-            } else if (data.originalFiles[0]['size'] && data.originalFiles[0]['size'] > 2000000) {
-                error.message = "File type too big";
+                return;
+            }
+            if (data.originalFiles[0]['size'] && data.originalFiles[0]['size'] > 2000000) {
+                error.message = 'Filesize is too big';
                 showPopup(error);
+                return;
+            }
+            else{
+                data.submit();
             }
         },
         dataType: 'json',
+        autoUpload: false,
         headers: {
             "session-key": localStorage["session_key"]
         },
         formData: {
             example: 'test'
         },
+        // maxFileSize: 2000000,
+        // acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
         done: function(e, data) {
             $("#display-pic").attr('src', data.result.thumbnail_url);
-        }
+        },
+        fail: function(e, data) {
+            $.each(data.messages, function(index, error) {
+                console.log(error);
+            });
+        },
     });
 
     $(".choose-pic-overlay").on('click', function() {
