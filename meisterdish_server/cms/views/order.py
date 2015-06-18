1df703f9bafdc930b30551ae0b1f41e2a3f1a648
Abdul Nasar <nasarp@qburst.com>
2015-06-18 18:42:47 +0530

    asd

--- meisterdish_server/cms/views/order.py
+++ meisterdish_server/cms/views/order.py
@@ -106,7 +106,7 @@ def get_orders(request, data, user):
             q &= Q(delivery_time__year=date_obj.year) & Q(delivery_time__month=date_obj.month) & Q(delivery_time__day=date_obj.day)            
 
         orders = orders.filter(q)
-        log.info(orders.query)
+        
         # End filter
         orders = orders.order_by("-id")
 
