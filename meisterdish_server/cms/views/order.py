f9311f06f19f5a9b8be2b5b61ef7ddfcb25b2bf9
Abdul Nasar <nasarp@qburst.com>
2015-06-22 19:40:50 +0530

    asd

--- meisterdish_server/cms/views/order.py
+++ meisterdish_server/cms/views/order.py
@@ -356,7 +356,7 @@ def send_sms_notification(dic):
         
         client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-        country_code = "+1" if settings.Live else "+1"
+        country_code = "+1" if settings.Live else "+91"
         number = country_code + str(dic["mobile"]).strip()
 
         message = client.messages.create(body=txt,
