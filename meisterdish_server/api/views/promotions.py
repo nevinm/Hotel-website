c14588655298f4134fde35c72b46de68beb70f37
Abdul Nasar <nasarp@qburst.com>
2015-06-24 19:13:50 +0530

    asd

--- meisterdish_server/api/views/promotions.py
+++ meisterdish_server/api/views/promotions.py
@@ -157,7 +157,7 @@ def get_cart_total(cart):
             for gc in cart.gift_cards.all():
                 discount += gc.amount
         if discount > total_price + total_tax:
-            discount = total_price + total_tax:
+            discount = total_price + total_tax
         return (total_price, total_tax, discount)
     except IOError as e:
         log.error("Cart total method :"+e.message)
