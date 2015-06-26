a1da726e993c761d6ed6a51cf65f5acdc5368e45
nevinm <nevinm@qburst.com>
2015-06-26 12:23:50 +0530

    Added dynamic link to share page.

--- web/js/share_page.js
+++ web/js/share_page.js
@@ -54,7 +54,7 @@ function popitup(url) {
 }
 
 function facebookShare(site_url, accessToken) {
-    var imgURL = "http://imgur.com/byNMcg1"; //change with your external photo url
+    var imgURL = homeUrl + "/images/fb_sharing.png"; //change with your external photo url
     FB.api('me/photos', 'post', {
         message: 'Ready to cook meals, delivered on demand.' +
             'Start cooking today' +
