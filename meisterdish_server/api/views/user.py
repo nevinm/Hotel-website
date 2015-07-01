d678745030fe1d91e59b07f9724a0671777afc1b
Abdul Nasar <nasarp@qburst.com>
2015-07-01 22:10:31 +0530

    asd

--- meisterdish_server/api/views/user.py
+++ meisterdish_server/api/views/user.py
@@ -379,21 +379,22 @@ def share_via_email(request, data, user):
 
 @check_input('POST')
 def save_email(request, data):
+    import mailchimp
     try:
-        from mailchimp import Mailchimp
         email = data["email"].strip()
         zip = str(data["zipcode"]).strip()  
         if not validate_email(email):
             return custom_error("Please enter a valid email.")
-        mc = mailChimp(settings.MAILCHIMP_API_KEY)
+        mc = mailchimp.Mailchimp(settings.MAILCHIMP_API_KEY)
         res = mc.lists.subscribe(settings.MAILCHIMP_LIST_ID, {'email': email})
         if res and res['euid']:
             log.info("Added email to list")
         else:
             log.info("Failed to add email to list")
-            return json_response({"status":1, "message":"Your email has been recorded. You will be notified when delivery becomes available at your location."})
+        return json_response({"status":1, "message":"Your email has been recorded. You will be notified when delivery becomes available at your location."})
     except mailchimp.ListAlreadySubscribedError:
+        log.info("Email already added to list")
         return json_response({"status":1, "message":"Your email has already been recorded. You will be notified when delivery becomes available at your location."})
-    except Exception as e:
+    except KeyError as e:
         log.error("Save email :"+ e.message)
         return custom_error("An error has occurred. Please try again later.")        
