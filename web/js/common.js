0eef4ff37376625e376c1dfecc82b6f46d1d8c32
Nevin <nevinm@qburst.com>
2015-07-22 16:00:10 +0530

    Updated for Twitter

--- web/js/common.js
+++ web/js/common.js
@@ -201,6 +201,7 @@ function getParameterFromUrl(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+
 function getCurrentPage(firstChar, secondChar, url) {
     currentPage = url.substring(url.lastIndexOf(firstChar) + 1, url.lastIndexOf(secondChar));
     return currentPage;
@@ -250,6 +251,7 @@ function logingOut() {
     localStorage.removeItem('delivery_addressess');
     localStorage.removeItem('fb-image');
     localStorage.removeItem('admin_role');
+    localStorage.removeItem('referral_code');
     localStorage['loggedIn'] = false;
     localStorage['admin_loggedIn'] = false;
     $('#navbar-username a').text('');
--- web/js/redirect.js
+++ web/js/redirect.js
@@ -1,3 +1,14 @@
+function getrefferallFromUrl(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+if(getrefferallFromUrl("refferalCode").length){
+	window.location.replace("signup.html?ref=" + refferalCode);
+}else{}
+
 function redirectIfNotLoggedIn(){
 	if(document.title=="Meisterdish - Log in"){
 		if (localStorage['loggedIn'] == 'true' || localStorage['admin_loggedIn'] == 'true') {
