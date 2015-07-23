8844408ec1d098992eb5ad7e7255dbd06aef46c9
Abdul Nasar <nasarp@qburst.com>
2015-07-23 11:07:16 +0530

    removed order listing constraint

--- meisterdish_server/api/views/order.py
+++ meisterdish_server/api/views/order.py
@@ -21,7 +21,7 @@ def get_orders(request, data, user):
         page = data.get("nextPage",1)
                     
         order_list = []
-        orders = Order.objects.filter(is_deleted=False, cart__user=user).exclude(status=0)
+        orders = Order.objects.filter(is_deleted=False, cart__user=user)
         
         total_count = orders.count()
 
