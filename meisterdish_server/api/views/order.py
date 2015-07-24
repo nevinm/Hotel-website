b706a64c63b7902d70e406e6465d483d17a09f17
Abdul Nasar <nasarp@qburst.com>
2015-07-24 16:16:27 +0530

    fix for FTR0045

--- meisterdish_server/api/views/order.py
+++ meisterdish_server/api/views/order.py
@@ -401,14 +401,14 @@ def get_order_cart_items(order):
               coupon = {
                 "code" : cart.promo_code.code,
                 "amount":cart.promo_code.amount,
-                "message":"Discount of $ "+str(cart.promo_code.amount) + " has been applied to your cart."
+                "message":"Discount of $"+str(cart.promo_code.amount) + " has been applied to your cart."
               }
           elif cart.gift_cards.all().count():
               gc = cart.gift_cards.all()[0]
               coupon = {
                 "code" : gc.code,
                 "amount":gc.amount,
-                "message":"Discount of $ "+str(gc.amount) + " has been applied to your cart."
+                "message":"Discount of $"+str(gc.amount) + " has been applied to your cart."
               }
 
       if not len(cart_list):
