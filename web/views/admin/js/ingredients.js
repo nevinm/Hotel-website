fa3c68be3c0036d69161c4675b804a7becef73a0
Nevin <nevinm@qburst.com>
2015-07-29 10:44:45 +0530

    Initial commit for  ingredients

---
+++ web/views/admin/js/ingredients.js
@@ -0,0 +1,29 @@
+// Ingredients add
+var listIngredientsCallback = {
+    success: function(data, textStatus) {
+        var mealManageData = JSON.parse(data);
+        if (mealManageData.status == -1) {
+            showCallBackStatusPre();
+            showPopup(mealManageData);
+        } else {
+            getMealtypes(1);
+        }
+    },
+    failure: function(XMLHttpRequest, textStatus, errorThrown) {}
+}
+
+function listIngredients(ingredientSearch) {
+    var url = baseURL + 'cms/list_ingredients/';
+    header = {
+            "session-key": localStorage['session_key']
+        },
+        params = {
+            "search" : 
+        };
+    if (mealTypeId) {
+        params['attribute_id'] = mealTypeId;
+    }
+    data = JSON.stringify(params);
+    var listIngredients = new AjaxHttpSender();
+    listIngredients.sendPost(url, header, data, listIngredientsCallback);
+}
