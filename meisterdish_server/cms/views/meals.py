056caee74b3cb096bd7c1f0dcc4ac24ed5707c89
Abdul Nasar <nasarp@qburst.com>
2015-07-30 10:34:59 +0530

    fix for UAT bugs 25,26

--- meisterdish_server/cms/views/meals.py
+++ meisterdish_server/cms/views/meals.py
@@ -336,7 +336,7 @@ def get_meal_details(request, data, user, meal_id):
         return json_response({
             "status":1,
             "id" : meal.id,
-            "name" : meal.name.title(),
+            "name" : meal.name,
             "sub":meal.sub,
             "description" : meal.description,
             "price":meal.price,
