64f87de4d486359a73287c63ab6613813893ed69
nabeel <nabeel@qburst.com>
2015-08-04 10:17:15 +0530

    param name changed in yurl

--- web/js/unsubscribed.js
+++ web/js/unsubscribed.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	CartItemCount();
-	var result  = getParameterFromUrl("status");
+	var result  = getParameterFromUrl("subscribe");
 	if(result == "error"){
 		$("p.main-header").text("You are not subscribed.")
 	}
