94c617188fa65f047d3349e8553b785c0c3e246e
Jawahir Kottammal <jawahir@qburst.com>
2016-01-21 15:30:31 +0530

    Fixed Bugs #241602, #241609

--- web/app/js/app/order-confirmation.js
+++ web/app/js/app/order-confirmation.js
@@ -1,5 +1,6 @@
-(function(){
+$(document).ready(function () {
 	localStorage.removeItem('orderConfirm');
 	//Facebook pixel for tracking
 	fbq('track', 'Purchase', {'value': cartValue, 'currency': 'USD'});
-})();
+	CartItemCount();
+});
